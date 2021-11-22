c842856ab3d1e348fa025bddeb4d0cb5968f6780
Muhammad Numair Qureshi <muhammadadnan_qureshi@yahoo.com>
2021-11-22 12:49:00 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:calculator/home.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Calculator",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SimpleCalculator(),
+    );
+  }
+}
